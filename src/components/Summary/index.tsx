652f0c5613548f31e58a75035e774f4bce0e83af
Rosângela Nery <rosangela2nery@gmail.com>
2023-01-30 16:50:44 -0300

    Creating transaction context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,11 @@
 import { SummaryCar, SummaryContainer } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
     return (
         <SummaryContainer>
             <SummaryCar>
