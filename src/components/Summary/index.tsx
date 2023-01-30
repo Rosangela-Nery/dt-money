486442a895482995d7207c261abffa2b95ba4561
Rosângela Nery <rosangela2nery@gmail.com>
2023-01-30 17:09:40 -0300

    Calculating transaction summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,27 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce((acc, transaction) => {
+    /*Se a minha transaction.type === 'income' eu vou pegar o acc.income e vou somar com o transaction.price se não, vou pegar o acc.outcome e vou somar com o transaction.price*/
+    if(transaction.type === 'income') {
+      acc.income += transaction.price;
+
+      /*Quando for uma entrada eu vou aumentar o total com o preço da transação*/
+      acc.total += transaction.price;
+    } else {
+      acc.outcome += transaction.price;
+
+            /*Quando for uma saída eu vou diminuir o total usando o preço da transação*/
+      acc.total -= transaction.price;
+    }
+    return acc;
+  }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
     return (
         <SummaryContainer>
             <SummaryCar>
@@ -13,7 +34,7 @@ export function Summary() {
                   <span>Entradas</span>
                   <ArrowCircleUp size={32} color="#00b37e" />
               </header>
-              <strong>R$ 17.400,00</strong>
+              <strong>R$ {summary.income}</strong>
             </SummaryCar>
 
             <SummaryCar>
@@ -21,7 +42,7 @@ export function Summary() {
                   <span>Saídas</span>
                   <ArrowCircleDown size={32} color="#f75a68" />
               </header>
-              <strong>R$ 17.400,00</strong>
+              <strong>R$ {summary.outcome}</strong>
             </SummaryCar>
 
             <SummaryCar variant='green'>
@@ -29,7 +50,7 @@ export function Summary() {
                   <span>Total</span>
                   <CurrencyDollar size={32} color="#fff" />
               </header>
-              <strong>R$ 17.400,00</strong>
+              <strong>R$ {summary.total}</strong>
             </SummaryCar>
         </SummaryContainer>
     )
